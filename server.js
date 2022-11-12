3cbab670b700aaa48045be43c429a7608495cc6c
Dana Wyatt <danaatemca@aol.com>
2022-11-12 11:51:17 -0600

    Cleaned up code

--- server.js
+++ server.js
@@ -1,133 +1,81 @@
-let express = require('express');
+let express = require("express");
 let fs = require("fs");
-let cors = require('cors');
+let cors = require("cors");
+
 let app = express();
-
-
-app.use(express.json())
-app.use(cors())
+app.use(cors());
 
 // Create application/x-www-form-urlencoded parser
-let urlencodedParser = express.urlencoded({ extended: false })
+let urlencodedParser = express.urlencoded({ extended: false });
+app.use(express.json());
 
-function logOneCourse(course)
-{
-    console.log("ID: " + course.id + 
-                " " + course.dept + " " + course.courseNum + 
-                " Name:" + course.courseName + 
-                " Instructor:" + course.instructor + 
-                " Starts:" + course.startDate +
-                " Num Days:" + course.numDays);
-}
+////////////////////////////////////////////////////////////
+// API endpoints
 
-function logArrayOfCourses(arr)
-{
-    for(let i=0; i < arr.length; i++)
-    {
-        logOneCourse(arr[i])
-    }
-}
+// GET all courses
+app.get("/api/courses", function (req, res) {
+    console.log("LOG: Got a GET request for all courses");
 
-app.get('/', function (req, res) {
-   console.log("Got a GET request at /");
-   res.send('Hello World');
-})
-
-app.get('/api/courses', function (req, res) {
-    console.log("Got a GET request for courses");
-    let data = fs.readFileSync( __dirname + "/data/" + "courses.json", 'utf8');
+    let data = fs.readFileSync(__dirname + "/data/" + "courses.json", "utf8");
     data = JSON.parse(data);
-    console.log( "Returned data is: ");
-    logArrayOfCourses(data);
-    res.end( JSON.stringify(data) );
+
+    // LOG returned data
+    console.log("LOG: Returned courses -> ");
+    console.log(data);
+
+    res.end(JSON.stringify(data));
 });
 
-app.get('/api/courses/:id', function (req, res) {
+// GET one course by id
+app.get("/api/courses/:id", function (req, res) {
     let id = req.params.id;
-    console.log("Got a GET request for course " + id);
-    let data = fs.readFileSync( __dirname + "/data/" + "courses.json", 'utf8');
+    console.log("LOG: Got a GET request for course " + id);
+
+    let data = fs.readFileSync(__dirname + "/data/" + "courses.json", "utf8");
     data = JSON.parse(data);
-    console.log( "In callback after read");
-    let match = data[id - 1];
-    console.log( "Returned data is: " );
-    logOneCourse(match);
-    res.end( JSON.stringify(match) );
-})
+
+    // Find the course
+    let match = data.find(c => c.id == id);
+
+    // If course not found
+    if (match == undefined) {
+        console.log("LOG: **NOT FOUND**: course " + id + " does not exist!");
+        res.status(404).send();   // not found
+        return;
+    }
+
+    // LOG returned data
+    console.log("LOG: Returned course -> ");
+    console.log(match);
+
+    res.end(JSON.stringify(match));
+});
 
 /*
- *   This version uses query string parameters
- *   and just echos the received data
- *
-app.post('/api/courses', function (req, res) {
-    console.log("Got a POST request for courses");
-
-    // Prepare output in JSON format
-    let course = {
-        id:req.query.id,
-        dept:req.query.dept,        
-        courseNum:req.query.coursenum,
-        courseName:req.query.coursename,
-        instructor:req.query.instructor,
-        startDate:req.query.starts,
-        numDays:req.body.numdays
-    };
-    logOneCourse(course);
-    res.end(JSON.stringify(course));
- })
- */
-
-app.post('/api/courses', urlencodedParser, function (req, res) {
-    console.log("Got a POST request for courses");
-    console.log("BODY -------->" + JSON.stringify(req.body));
-
-    let data = fs.readFileSync( __dirname + "/data/" + "courses.json", 'utf8');
-    data = JSON.parse( data );
-
-    console.log( "Original data: " );
-    logArrayOfCourses(data);
-
-    let course = {
-        id:data.length + 1,
-        dept:req.body.dept,        
-        courseNum:req.body.coursenum,
-        courseName:req.body.coursename,
-        instructor:req.body.instructor,
-        startDate:req.body.starts,
-        numDays:req.body.numdays
-    };
-    console.log( "New course: " );
-    logOneCourse(course);
-
-    data[data.length] = course;
-
-    console.log( "New data after add: " );
-    logArrayOfCourses(data);
-
-    fs.writeFileSync(__dirname + "/data/" + "courses.json", JSON.stringify(data));
-   
-    console.log('New course saved!');
+app.post("/api/courses", urlencodedParser, function (req, res) {
+    console.log("LOG: Got a POST request for courses");
+    console.log("LOG: Mesage body --------> ");
+    console.log(JSON.stringify(req.body));
     res.status(201).json(course);
  })
 
-
-
- /*
- app.put('/api/courses', function (req, res) {
-    console.log("Got a PUT request for courses");
-    res.send('Courses PUT');
+ app.put("/api/courses/:id", function (req, res) {
+    console.log("LOG: Got a PUT request for a course");
+    res.status(200).send();
  })
  
- app.delete('/api/courses', function (req, res) {
-    console.log("Got a DELETE request for courses");
-    res.send('Courses DELETE');
+ app.delete("/api/courses/:id", function (req, res) {
+    console.log("LOG: Got a DELETE request for a course");
+    res.status(200).send();
  })
- */
- 
-app.use(express.static('public'));
+*/
+
+/////////////////////////////////////////////////////
+// Start the server
+
+// app.use(express.static("public"));
 
 let server = app.listen(8081, function () {
-   //let host = server.address().address
-   let port = server.address().port
-   
-   console.log("App listening at port %s", port)
-})
+    let port = server.address().port;
+    console.log("LOG: App listening at port %s", port);
+});
